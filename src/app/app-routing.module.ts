8b4db1d4bf8534f4ab47d55177f60a3ed06f182c
Rafael Mendez Asencio <rmendez1972@hotmail.com>
2017-08-17 12:44:58 -0500

    actualizando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/index';
-import { BotonBuscarComponent } from './boton-buscar/boton-buscar.component';
+//import { BotonBuscarComponent } from './boton-buscar/boton-buscar.component';
 import { MenuComponent } from './menus/menu.component';
 import { LoginComponent } from './login/index';
-import { RegisterComponent } from './register/index';
+//import { RegisterComponent } from './register/index';
 import { AuthGuard } from './_guards/index';
 
 // vector vacio por q las rutas las controlo en cada componente
 const appRoutes: Routes = [
     { path: '',  redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'home', component: HomeComponent }
+    //{ path: 'register', component: RegisterComponent }
 
 ];
 
