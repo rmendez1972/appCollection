92032f62559312f8a87474efad6ec3693e94fc2b
marlongomez <marlonegomezh@gmail.com>
2018-04-05 11:38:21 -0500

    modulo de aplica_mov_diversos impresion de datos

--- src/app/mov_diversos/aplica_mov_diversos-list.component.ts
+++ src/app/mov_diversos/aplica_mov_diversos-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, HostBinding, trigger, transition, animate, style, st
 
 import { Aplica_Mov_diversos } from './aplica_mov_diversos';
 import { Benef_div } from './benef_div';
+import { Mov_diversos } from './mov_diversos';
 import { Aplica_Mov_diversosService} from './aplica_mov_diversos.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
@@ -54,9 +55,16 @@ export class Aplica_Mov_diversosListComponent implements OnInit {
   private errorMessage: string;
   model:any={};
   private aplcia_mov_diversos: Aplica_Mov_diversos[];
+  private mov_diversos: Mov_diversos[];
   private benef_div: Benef_div[];
-  private k: Observable<Aplica_Mov_diversos[]>;
+  private k: Observable<Mov_diversos[]>;
   private l: Observable<Benef_div[]>;
+  private miMensajeerrorMovs:String;
+  private miMensajeMovs:String;
+  private miMensajeBenef:string;
+
+  private aplicado:String = "fa fa-check";
+  private noaplicado: String = "fa fa-times";
 
 
   optionsSelect = [
@@ -88,8 +96,64 @@ private seleccionado:String="clave_b";
     localizaBenefMov_diversos(){
       console.log('valor de model.criterio '+this.model.criterio);
       console.log('valor de model.valorcriterio '+this.model.valorcriterio);
+      if ((this.model.criterio!=undefined) && (this.model.valorcriterio!=null )){
+        this.miMensajeerrorMovs=null;
+        this.getMov_diversos(this.model.criterio,this.model.valorcriterio);
+        this.getBenef_div(this.model.criterio,this.model.valorcriterio);
+        
+      }else{
+        this.miMensajeMovs=null;
+        this.miMensajeerrorMovs = "Error en recuperación de Movimientos diversos, por favor llena los campos..";
+      }
       
     }
+
+    getMov_diversos(criterio:String,valorcriterio:String) {
+      this.k=this.route.params
+      // (+) converts string 'id' to a number
+      .switchMap((params: Params) =>
+      {
+        return this.aplica_mov_diversosservice.getMov_diversos(criterio,valorcriterio)
+      })
+
+      this.k.subscribe(
+        
+        movimientos => {
+          this.mov_diversos = movimientos;  
+          
+          this.miMensajeMovs = "Recuperación Exitosa de los Movimientos diversos";
+         },
+        error =>  this.errorMessage = <any>error);
+
+        //movimientos => this.mov_diversos = movimientos,
+        //error =>  this.errorMessage = <any>error);
+    };
+
+  getBenef_div(criterio:String,valorcriterio:String) {
+      this.l=this.route.params
+      // (+) converts string 'id' to a number
+      .switchMap((params: Params) =>
+      {
+        //this.selectedId= +params['id'];
+           return this.aplica_mov_diversosservice.getBenef_div(criterio,valorcriterio)
+      })
+
+      this.l.subscribe(
+
+                     beneficiario => {
+                       this.benef_div = beneficiario;
+                     
+                     if (this.benef_div.length>0){
+                       console.log("Encontrado!!!!");
+                      this.miMensajeBenef="Encontrado";
+                     }else {this.miMensajeBenef=null}
+                    },
+                     error =>  this.errorMessage = <any>error);
+   
+
+  };
+
+
 
     
 
